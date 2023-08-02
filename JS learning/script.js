16031e032b65bd78d6cde3dcdfda5c842d5913fe
FEROZMEMON <ferozmemon726@gmail.com>
2023-08-02 07:52:38 +0500

    learnig java script

---
+++ JS learning/script.js
@@ -0,0 +1,10 @@
+var chemsitry = 90
+var physics = 80
+var maths = 90
+var urdu = 85
+var english = 60
+var islamiat = 95 
+var totalmarks = 600
+var obtained = (chemsitry + physics + maths + urdu + english + islamiat)
+var percentage = (obtained/totalmarks*100) 
+console.log(percentage) 
