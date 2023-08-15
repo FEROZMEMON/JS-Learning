bb5f2bb2e08fb9b283321c87deb75a3ba693978f
FEROZMEMON <ferozmemon726@gmail.com>
2023-08-15 12:21:14 +0500

    learning ....

---
+++ script.js
@@ -0,0 +1,49 @@
+// var chemsitry = 90
+// var physics = 80
+// var maths = 90
+// var urdu = 85
+// var english = 60
+// var islamiat = 95 
+// var totalmarks = 600
+// var obtained = (chemsitry + physics + maths + urdu + english + islamiat)
+// var percentage = (obtained/totalmarks*100) 
+// console.log(percentage) 
+
+
+
+
+
+ let names = "feroz"
+ let age = 20
+//  console.log("my name is" + " " + names +" " + "and my age is" +" " + age   )
+document.write ("my name is" + " " + names + " " + "and my age is" + " " + age) 
+
+
+
+
+// var checkNum = 10 > 5 && 10 < 5 || 15 > 10;
+// console.log(checkNum);
+
+
+
+
+// let num2 = 20
+// console.log(num2*=30);
+// console.log(num2 * 30);
+
+// let feroz = "10"
+// let khubaib = 10
+// console.log(feroz > khubaib ); 
+// console.log(feroz >! khubaib ); 
+// console.log(typeof(khubaib));
+
+// let haris = +prompt("enter your name")
+// let usman = +prompt("enter your name") 
+// console.log(haris - usman);
+
+
+
+
+  
+
+
