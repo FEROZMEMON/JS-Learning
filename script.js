86dc8f95f88d5b46b72c495b8b15afadc593c9f0
FEROZMEMON <ferozmemon726@gmail.com>
2023-08-16 08:51:23 +0500

    if else and switch done

--- script.js
+++ script.js
@@ -13,10 +13,10 @@
 
 
 
- let names = "feroz"
- let age = 20
+let names = "feroz"
+let age = 20
 //  console.log("my name is" + " " + names +" " + "and my age is" +" " + age   )
-document.write ("my name is" + " " + names + " " + "and my age is" + " " + age) 
+document.write("my name is" + " " + names + " " + "and my age is" + " " + age)
 
 
 
@@ -41,9 +41,149 @@ document.write ("my name is" + " " + names + " " + "and my age is" + " " + age)
 // let usman = +prompt("enter your name") 
 // console.log(haris - usman);
 
+// let num = 10
+// console.log(num);
+// let ans = num
+// console.log(ans);
 
 
 
-  
+// let physics = +prompt("enter your physics marks")
+// let chemsitry = +prompt("enter your chemistry marks")
+// let maths = +prompt("enter your maths marks")
+// let english = +prompt("enter your english marks")
+// let islamiat = +prompt("enter your islamiat marks")
+// let urdu = +prompt("enter your urdu marks")
+
+// let obtained = (physics+chemsitry+maths+english+islamiat+urdu)
+// let totalmarks = 600
+// let percentage = (obtained/totalmarks*100)
+// console.log(percentage+"%");
+
+
+
+
+
+
+// let percentage = +prompt("enter your percentage")
+// if (percentage >= 80) {
+//     console.log("A+")
+// } else if(percentage >= 70){
+//     console.log("A grade")
+// } else if(percentage >= 60){
+//     console.log("B grade")
+// } else if(percentage >= 40){
+//     console.log("C grade")
+// } else {
+//     console.log("BHAI TUM FAIL HO GYE")
+// }
+
+
+
+//  let num = 30
+// if (num < 20) {
+//     console.log("ok");
+// }
+//     else if (num>20) {
+//         console.log("okay");
+// }
+// else{
+//     console.log("not ok");
+// }
+
+// let num1 = 10;
+// if (11 < num1 || 11 > num1 || 15 < num1) {
+//     console.log("if chal rha ha bhai");
+//     if(9>num1){
+//         console.log("if ka undar wala if")
+//     }
+//     else{
+//         console.log("if ka undar wala else")
+//     }
+
+// } else {
+//     console.log("else chal rha ha bhai");
+//     if(9 < num1){
+//         console.log("else wala if")
+//     }
+//     else{
+//         console.log("else wala else")
+//     }
+// }
+
+
+
+
+// let num = 10
+// switch (true) {
+//     case ( num + 3==13):
+//         console.log("ok");
+//         break;
+
+//     default:
+//         console.log("not ok");
+// }
+
+// let num1 = 10;
+// switch (true) {
+//     case (num1 < 5):
+//         console.log("pehla case chal gaya");
+//          break 
+//     case (num1 > 6):
+//         console.log("dosra case");
+//         break
+//     default:
+//         console.log("default chal gaya");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
